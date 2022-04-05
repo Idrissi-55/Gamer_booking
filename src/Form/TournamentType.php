107aaaf337f71f4bdbb7dfce4a9d0baa5bbd17d6
Aymeric512 <aymeric.gautier2020@campus-eni.fr>
2022-04-05 09:06:41 +0200

    tournament crud, form theme, base.html.twig update

---
+++ src/Form/TournamentType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Tournament;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TournamentType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('starting_date', DateType::class, ['widget'=>'single_text'])
+            ->add('ending_date', DateType::class, ['widget'=>'single_text'])
+            ->add('Award', MoneyType::class, [
+                'divisor' => 100,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Tournament::class,
+        ]);
+    }
+}
